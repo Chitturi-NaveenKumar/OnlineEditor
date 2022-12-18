b0c5af1ed5dc2fd829d90e6fb57021cda8cc119a
Chitturi-NaveenKumar <chitturi.kumar1@gmail.com>
2022-12-18 15:22:36 +0530

    Updates

---
+++ precache-manifest.9a09870e115f4c2aa3891e971be7795b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74104b74a865f4cbfff85635110e55dd",
+    "url": "/OnlineEditor/index.html"
+  },
+  {
+    "revision": "20e37c08395dad1e8c73",
+    "url": "/OnlineEditor/static/css/2.8e69e9c3.chunk.css"
+  },
+  {
+    "revision": "97b6e3695637788e6f0d",
+    "url": "/OnlineEditor/static/css/main.d630cba6.chunk.css"
+  },
+  {
+    "revision": "20e37c08395dad1e8c73",
+    "url": "/OnlineEditor/static/js/2.574d5bce.chunk.js"
+  },
+  {
+    "revision": "4d74ce1e7fee0a16b28267ca01d74155",
+    "url": "/OnlineEditor/static/js/2.574d5bce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b6e3695637788e6f0d",
+    "url": "/OnlineEditor/static/js/main.49bdd7f3.chunk.js"
+  },
+  {
+    "revision": "905a25e9c366aea10ea6",
+    "url": "/OnlineEditor/static/js/runtime-main.90f327cd.js"
+  },
+  {
+    "revision": "f0d92a6a17eb82f4094cd5cfe30942e8",
+    "url": "/OnlineEditor/static/media/css.f0d92a6a.png"
+  }
+]);
